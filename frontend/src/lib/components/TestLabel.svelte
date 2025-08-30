b35c10900b5d9cd9f54db0964538d46b67b40be4
Ronni Skansing <rskansing@gmail.com>
2025-08-30 08:31:39 +0200

    fix: smaller row height

--- frontend/src/lib/components/TestLabel.svelte
+++ frontend/src/lib/components/TestLabel.svelte
@@ -1,6 +1,3 @@
-<span
-	title="Test / Preview"
-	class="select-none border-2 pl-1 pr-1 bg-white relative -top-3 rounded-xl text-xs"
->
+<span title="Test / Preview" class="select-none px-1 border-2 relative -top-1 rounded-lg text-xs">
 	test
 </span>
--- frontend/src/lib/components/table/TableRow.svelte
+++ frontend/src/lib/components/table/TableRow.svelte
@@ -1,3 +1,3 @@
-<tr class="table-row odd:bg-white even:bg-pleasant-gray h-16 hover:bg-cta-light-blue">
+<tr class="table-row odd:bg-white even:bg-pleasant-gray h-10 hover:bg-cta-light-blue">
 	<slot />
 </tr>
