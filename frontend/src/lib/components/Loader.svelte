50203e5c8a666138f7f7f693f21bd90b19b64ba4
Ronni Skansing <rskansing@gmail.com>
2025-10-02 11:02:43 +0200

    fix loader overlay
    Signed-off-by: Ronni Skansing <rskansing@gmail.com>

--- frontend/src/lib/components/Loader.svelte
+++ frontend/src/lib/components/Loader.svelte
@@ -15,16 +15,13 @@
 </script>
 
 {#if $isLoading && isAnimating}
-	<div
-		class="fixed top-0 left-0 w-full h-full bg-black dark:bg-gray-900 opacity-[0.5] transition-colors duration-200"
-		transition:blur={{ duration }}
-	/>
+	<div class="fixed top-0 left-0 w-full h-full opacity-[0.5]" transition:blur={{ duration }} />
 	<div
 		transition:blur={{ duration }}
 		class="fixed top-0 left-0 w-full h-full flex justify-center items-center backdrop-blur-sm z-50"
 	>
 		<div
-			class="w-20 h-20 border-t-8 border-t-cta-blue dark:border-t-blue-500 border-r-8 border-r-cta-blue dark:border-r-blue-500 border-b-cta-blue dark:border-b-blue-500 border-b-8 border-l-transparent border-l-8 rounded-full animate-spin transition-colors duration-200"
+			class="w-20 h-20 border-t-8 border-t-cta-blue border-r-8 border-r-cta-blue border-b-cta-blue border-b-8 border-l-transparent border-l-8 rounded-full animate-spin"
 		></div>
 	</div>
 {/if}
