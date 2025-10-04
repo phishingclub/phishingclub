0f7e9dba5ca4810ba8922fb703ea2d95a79fb643
Ronni Skansing <rskansing@gmail.com>
2025-10-04 22:46:03 +0200

    fix missing darkmode corners on TextFieldMultiSelect
    Signed-off-by: Ronni Skansing <rskansing@gmail.com>

--- frontend/src/lib/components/TextFieldMultiSelect.svelte
+++ frontend/src/lib/components/TextFieldMultiSelect.svelte
@@ -175,7 +175,7 @@
 						{#each filteredOptions as option}
 							<li>
 								<button
-									class="w-full text-left bg-slate-100 dark:bg-gray-600 rounded-md text-gray-600 dark:text-gray-200 hover:bg-grayblue-dark dark:hover:bg-gray-500 hover:text-white py-2 px-2 cursor-pointer transition-colors duration-200"
+									class="w-full text-left bg-slate-100 dark:bg-gray-700 rounded-md text-gray-600 dark:text-gray-200 hover:bg-grayblue-dark dark:hover:bg-gray-500 hover:text-white py-2 px-2 cursor-pointer transition-colors duration-200"
 									on:click={() => {
 										onClickSelectedOption(option);
 									}}
@@ -186,7 +186,7 @@
 						{/each}
 					{:else}
 						<li
-							class="w-full bg-slate-100 dark:bg-gray-600 rounded-md text-gray-600 dark:text-gray-300 py-2 px-2 transition-colors duration-200"
+							class="w-full bg-slate-100 dark:bg-gray-700 text-gray-600 dark:text-gray-300 py-2 px-2 transition-colors duration-200"
 						>
 							List is empty
 						</li>
