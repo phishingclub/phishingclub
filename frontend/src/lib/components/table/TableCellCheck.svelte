e5e98f9939ab07f03c0b0baf5afcf53eb50f7f22
Ronni Skansing <rskansing@gmail.com>
2025-10-29 23:56:57 +0100

    fix bad border-hidden rule
    Signed-off-by: Ronni Skansing <rskansing@gmail.com>

--- frontend/src/lib/components/table/TableCellCheck.svelte
+++ frontend/src/lib/components/table/TableCellCheck.svelte
@@ -2,7 +2,7 @@
 	export let value = false;
 </script>
 
-<td class="pl-4 w-40 text-center border: hidden;">
+<td class="pl-4 w-40 text-center border-hidden">
 	<p
 		class="font-regular text-slate-600 dark:text-gray-200 flex justify-center transition-colors duration-200"
 	>
--- frontend/src/lib/components/table/TableCellEmpty.svelte
+++ frontend/src/lib/components/table/TableCellEmpty.svelte
@@ -1,2 +1,2 @@
-<td class="pl-4 w-4 border: hidden; bg-white dark:bg-gray-900/40 transition-colors duration-200">
+<td class="pl-4 w-4 border-hidden bg-white dark:bg-gray-900/40 transition-colors duration-200">
 </td>
