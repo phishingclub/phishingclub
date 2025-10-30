d727bc4b7378c024d212adcdc7058101681f1d24
Ronni Skansing <rskansing@gmail.com>
2025-10-30 08:41:53 +0100

    fix bad table layout on firefox
    Signed-off-by: Ronni Skansing <rskansing@gmail.com>

--- frontend/src/lib/components/table/TableCellAction.svelte
+++ frontend/src/lib/components/table/TableCellAction.svelte
@@ -1,4 +1,4 @@
-<td class="w-48 text-center border-hidden">
+<td class="text-center border-hidden">
 	<p class="font-regular text-slate-600 dark:text-gray-200 transition-colors duration-200">
 		<slot />
 	</p>
--- frontend/src/lib/components/table/TableHeadCellEmpty.svelte
+++ frontend/src/lib/components/table/TableHeadCellEmpty.svelte
@@ -1,2 +1,4 @@
-<th class="pl-4 bg-white dark:bg-gray-900 w-4 py-4 border-hidden transition-colors duration-200">
+<th
+	class="pl-4 bg-white dark:bg-gray-900 min-w-4 w-4 py-4 border-hidden transition-colors duration-200"
+>
 </th>
