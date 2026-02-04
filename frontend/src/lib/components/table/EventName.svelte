2a2ffb898b8087d74ad2c0ba4c93d508107578d5
Ronni Skansing <rskansing@gmail.com>
2026-02-04 21:51:37 +0100

    fix event box not aligned
    Signed-off-by: Ronni Skansing <rskansing@gmail.com>

--- frontend/src/lib/components/table/EventName.svelte
+++ frontend/src/lib/components/table/EventName.svelte
@@ -7,8 +7,8 @@
 </script>
 
 <div
-	class="flex items-center text-ellipsis text-gray-900 dark:text-gray-200 transition-colors duration-200"
+	class="!flex items-center text-ellipsis text-gray-900 dark:text-gray-200 transition-colors duration-200"
 >
-	<div class="w-4 h-4 {event.color} mr-2 rounded-sm"></div>
+	<div class="w-4 h-4 flex-shrink-0 {event.color} mr-2 rounded-sm"></div>
 	{event.name}
 </div>
