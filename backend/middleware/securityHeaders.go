b1bace4cce8f8a9af212f39989b0828154f8971b
Ronni Skansing <rskansing@gmail.com>
2026-06-05 13:56:15 +0200

    removed HSTS again due to self signed
    Signed-off-by: Ronni Skansing <rskansing@gmail.com>

--- backend/middleware/securityHeaders.go
+++ backend/middleware/securityHeaders.go
@@ -6,7 +6,6 @@ func SecurityHeaders() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		c.Header("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob:; connect-src 'self'; frame-src data:; font-src 'self'; object-src 'none'; base-uri 'self'; form-action 'self'; frame-ancestors 'none';")
 		c.Header("X-Content-Type-Options", "nosniff")
-		c.Header("Strict-Transport-Security", "max-age=63072000")
 		c.Header("Referrer-Policy", "strict-origin-when-cross-origin")
 		c.Header("Permissions-Policy", "camera=(), microphone=(), geolocation=(), payment=()")
 		c.Next()
